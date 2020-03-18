4986a254a4d35a180e717badf36117ea62ab0fd4
Rashīd Nexkoyotl <rashidtn1419@gmail.com>
2020-03-18 17:18:40 -0600

    Modularized and added links feature

--- xamix-element.js
+++ xamix-element.js
@@ -38,7 +38,12 @@ class XamixElement extends LitElement {
         reflect: true
       },
       parsedText: {type: Array},
-      textOutput: {type: Array}
+      textOutput: {type: Array},
+      links: {
+        type: Array,
+        reflect: true
+      },
+      stage: {type: String}
     };
   }
 
@@ -59,6 +64,15 @@ class XamixElement extends LitElement {
     this.unit = 'vw';
     this.textOutput = [];
     this.parsedText = [];
+    this.links = [];
+    this.stages = [
+      'on-construct',
+      'words-loading',
+      'words-loaded',
+      'words-resizing',
+      'words-resized'
+    ];
+    this.stage = this.stages[0];
   }
 
   setViewBox(svg, viewBox) {
@@ -149,51 +163,126 @@ class XamixElement extends LitElement {
   }
 
   parseText() {
+    this.uniqueCharacters = [];
+
     for (const text of this.textInput) {
-      const parsedWord = {word: text, syllables: []};
+      const parsedWord = {word: text};
 
-      if (text.includes('-')) {
+      if (text.includes('-'))
         parsedWord.components = text.split('-');
-
-        for (const syllable of parsedWord.components)
-          parsedWord.syllables = [...parsedWord.syllables, this.fetchSvg(syllable)];
-      } else
-        parsedWord.syllables = [this.fetchSvg(text)];
+      else
+        parsedWord.components = [text];
 
       this.parsedText = [...this.parsedText, parsedWord];
     }
+
+    this.parsedText.map(word => {
+      this.uniqueCharacters = [...this.uniqueCharacters, ...word.components];
+    });
+
+    this.uniqueCharacters = Array.from(new Set(this.uniqueCharacters));
+    this.uniqueCharacters = this.uniqueCharacters.map(char => {
+      return {name: char, file: this.fetchSvg(char)}
+    });
+  }
+
+  setUniqueCharactersContent(files) {
+    this.uniqueCharacters.forEach((char, index) => {
+      this.uniqueCharacters[index].file = files[index];
+    });
   }
 
   fetchSyllables() {
-    for (const word of this.parsedText)
-      Promise.all(word.syllables)
-        .then(files => {
-          word.textOutput = [];
-          for (const file of files)
-            word.textOutput = this.appendSvg(word.textOutput, file);
+    Promise.all(this.uniqueCharacters.map(char => char.file))
+      .then((files) => {
+        this.setUniqueCharactersContent(files);
+        this.formTextOutput();
+      });
+  }
 
-          word.content = document.createElement('div');
+  useWordAsLink(word, index) {
+    const link = this.links.filter(link => link.wordsIndexes.includes(index))[0];
 
-          for (const syllable of word.textOutput)
-            word.content.appendChild(syllable);
+    if (link) {
+      const linkContent = document.createElement('a');
 
-          word.content.classList.toggle('word');
+      linkContent.setAttribute('href', link.href);
+      linkContent.appendChild(word.content);
 
-          const contents = this.parsedText.map(word => word.content);
+      word.content = linkContent;
+    }
+  }
 
-          if (contents.length === this.parsedText.length)
-            for (const word of this.parsedText)
-              this.textOutput = [...this.textOutput, word.content];
-        });
+  setWordContent(word, index) {
+    word.content = document.createElement('div');
+
+    for (const syllable of word.textOutput)
+      word.content.appendChild(syllable);
+
+    word.content.classList.toggle('word');
+    this.useWordAsLink(word, index);
+  }
+
+  formWordOutput(word, index) {
+    word.textOutput = [];
+    let syllables = word.components
+      .map(component =>
+        this.uniqueCharacters.find(char => char.name === component).file);
+
+    for (const svgText of syllables)
+      word.textOutput = this.appendSvg(word.textOutput, svgText);
+
+    this.setWordContent(word, index);
+  }
+
+  finishTextOutput() {
+    this.textOutput = [...this.parsedText.map(word => word.content)];
+    this.stage = this.stages[2];
+
+    this.dispatchEvent(new Event('words-loaded'));
+  }
+
+  formTextOutput() {
+    this.parsedText.forEach((word, index) => this.formWordOutput(word, index));
+    this.finishTextOutput();
   }
 
   convertInput() {
     if (typeof this.textInput === 'string') {
+      this.stage = this.stages[1];
       this.textInput = this.textInput.toLowerCase().split(' ');
       this.parsedText = [];
 
       this.parseText();
       this.fetchSyllables();
+    }
+  }
+
+  dispatchStageEvent(stage, detail) {
+    if (!detail)
+      this.dispatchEvent(new Event(stage));
+    else
+      this.dispatchEvent(new CustomEvent(stage, {detail: detail}));
+  };
+
+  dispatchCurrentStage(detail) {
+    this.dispatchStageEvent(this.stage, detail);
+  };
+
+  resizeSvgs() {
+    const svgs = this.shadowRoot.querySelectorAll('svg');
+
+    if (this.stage === this.stages[2]) {
+      this.stage = this.stages[3];
+
+      this.dispatchCurrentStage();
+
+      for (const svg of svgs)
+        this.resizeSvg(svg);
+
+      this.stage = this.stages[4];
+
+      this.dispatchCurrentStage();
     }
   }
 
@@ -203,21 +292,16 @@ class XamixElement extends LitElement {
     const clazz = `text${' ' + (this.vertical ? 'vertical' : 'horizontal')}`;
 
     return html`
-    <div class="${clazz}" style="background-color: ${this.backgroundColor}; color: ${this.fillColor};">
-        <slot name="before"></slot>
-        ${this.textOutput}
-        <slot name="after"></slot>
-    </div>
-    `;
+      <div class="${clazz}" style="background-color: ${this.backgroundColor}; color: ${this.fillColor}; line-height: ${this.fontSize}${this.getUnit()};">
+          <slot name="before"></slot>
+          ${this.textOutput}
+          <slot name="after"></slot>
+      </div>
+      `;
   }
 
   updated(_changedProperties) {
-    const svgs = this.shadowRoot.querySelectorAll('svg');
-
-    for (const svg of svgs)
-      this.resizeSvg(svg);
-
-    this.dispatchEvent(new Event('update-done'));
+    this.resizeSvgs();
   }
 }
 
